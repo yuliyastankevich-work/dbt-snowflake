0f48fea1d0c7851a50bcefd8f12494412de787bf
yuliyastankevich-work <yuliya.stankevich@innowise.com>
2026-07-07 13:11:55 +0000

    create stage model for hgnc

---
+++ play_dbt/models/genes/staging/stg_hgnc.sql
@@ -0,0 +1,26 @@
+with raw_hgnc AS (
+SELECT 
+CAST(split_part("hgnc_id",':',2) AS INT) AS hgnc_id,
+"symbol" as gene_symbol,
+"name" as gene_name,
+"locus_group" as locus_group,
+"locus_type" as locus_type,
+"location" as chr_location,
+"alias_symbol" as alias_symbol,
+"alias_name" as alias_name,
+"prev_symbol" as prev_symbol,
+"prev_name" as prev_name,
+"gene_group" as gene_group,
+CAST("date_symbol_changed" AS DATE) AS date_symbol_changed,
+CAST("date_name_changed" AS DATE) AS date_name_changed,
+CAST("entrez_id" AS INT) AS entrez_id,
+"ensembl_gene_id" AS ensembl_id,
+"refseq_accession" AS refseq_id,
+"uniprot_ids" AS uniprot_id,
+"pubmed_id" AS pubmed_accession,
+"omim_id" AS omim_accession, 
+"mane_select" AS other_ids
+
+from PLAY_SHEMA.HGNC_TABLE
+WHERE "status" = 'Approved')
+select * from raw_hgnc
--- snow_queries/snow_sql.sql
+++ snow_queries/snow_sql.sql
@@ -9,3 +9,8 @@ show stages;
 
 select * from PLAY_SHEMA.HGNC_TABLE
 limit 10;
+
+show views in schema PLAY_SHEMA;
+
+select * from PLAY_SHEMA.stg_hgnc
+limit 10;

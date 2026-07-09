5c9a3242fcdca72479c6a87b3336c5e66b7d35cc
yuliyastankevich-work <yuliya.stankevich@innowise.com>
2026-07-09 10:14:18 +0000

    rebuild the dbt poject with new names on dev

--- play_dbt/models/genes/intermediate/int_hgnc.sql
+++ play_dbt/models/genes/intermediate/int_hgnc.sql
@@ -4,3 +4,25 @@ gene_symbol,
 gene_name,
 locus_group,
 locus_type,
+chr_location,
+case
+    when LOWER(chr_location) like 'mitochondrial%' then 'MT'
+    when regexp_like(chr_location, '^([0-9]+|X|Y|M|MT)([pq].*|$)')
+    then UPPER(regexp_substr(chr_location, '^([0-9]+|X|Y|M|MT)([pq].*|$)', 1, 1, 'i', 1))
+    else chr_location
+    end as chr,
+alias_symbol,
+alias_name,
+prev_symbol,
+prev_name,
+gene_group,
+date_symbol_changed,
+date_name_changed,
+entrez_id,
+ensembl_id,
+refseq_id,
+uniprot_id,
+pubmed_accession,
+omim_accession,
+other_ids
+FROM {{ ref('stg_hgnc')}}

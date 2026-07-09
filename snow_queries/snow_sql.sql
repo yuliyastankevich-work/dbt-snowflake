0370ccfb9be81945f0b2a7eddca0eee264d9737f
yuliyastankevich-work <yuliya.stankevich@innowise.com>
2026-07-09 10:14:35 +0000

    temp_sql modified

--- snow_queries/snow_sql.sql
+++ snow_queries/snow_sql.sql
@@ -286,3 +286,33 @@ insert into play_source.samples
 VALUES ('SMP-1001', 'ANL-5001', 'Lung Cancer', 'blood', 'ready', current_timestamp());
 
 select * from play_source.samples;
+
+
+SELECT
+hgnc_id,
+gene_symbol,
+gene_name,
+locus_group,
+locus_type,
+chr_location,
+case
+    when LOWER(chr_location) like 'mitochondrial%' then 'MT'
+    when regexp_like(chr_location, '^([0-9]+|X|Y|M|MT)([pq].*|$)')
+    then UPPER(regexp_substr(chr_location, '^([0-9]+|X|Y|M|MT)([pq].*|$)', 1, 1, 'i', 1))
+    else chr_location
+    end as chr,
+alias_symbol,
+alias_name,
+prev_symbol,
+prev_name,
+gene_group,
+date_symbol_changed,
+date_name_changed,
+entrez_id,
+ensembl_id,
+refseq_id,
+uniprot_id,
+pubmed_accession,
+omim_accession,
+other_ids
+FROM PLAY_SHEMA.stg_hgnc;
